7232f5fbb47c491d7e3874a1fef3de717ff0c7da
Paulina-Dyulgerska <paylina_st@yahoo.com>
2020-08-06 10:07:15 +0300

    Update Profile

--- Exercise_XML_Processing/09. XML-Processing-Product-Shop-Skeleton/ProductShop/ProductShopProfile.cs
+++ Exercise_XML_Processing/09. XML-Processing-Product-Shop-Skeleton/ProductShop/ProductShopProfile.cs
@@ -12,7 +12,8 @@ namespace ProductShop
                 .ForMember(x => x.Name, y => y.MapFrom(x => x.Name))
                 .ForMember(x => x.Price, y => y.MapFrom(x => x.Price))
                 .ForMember(x => x.BuyerFullName,
-                            y => y.MapFrom(x => x.Buyer.LastName != null ? $"{x.Buyer.FirstName} {x.Buyer.LastName}" : null))
+                            y => y.MapFrom(x => x.Buyer.LastName != null ? $"{x.Buyer.FirstName} {x.Buyer.LastName}" 
+                            : null))
                 .ReverseMap();
         }
     }

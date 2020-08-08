bf5d2bbadd8f41ff3435c3700afafe6efc2609b1
Paulina-Dyulgerska <paylina_st@yahoo.com>
2020-08-08 22:46:00 +0300

    Update

--- Exam/VaporStore/StartUp.cs
+++ Exam/VaporStore/StartUp.cs
@@ -15,19 +15,17 @@
 
             Mapper.Initialize(config => config.AddProfile<VaporStoreProfile>());
 
-            //ResetDatabase(context, shouldDropDatabase: true);
+            ResetDatabase(context, shouldDropDatabase: true);
 
             var projectDir = GetProjectDirectory();
 
-            //ImportEntities(context, projectDir + @"Datasets/", projectDir + @"ImportResults/");
+            ImportEntities(context, projectDir + @"Datasets/", projectDir + @"ImportResults/");
             ExportEntities(context, projectDir + @"ImportResults/");
 
             using (var transaction = context.Database.BeginTransaction())
             {
                 transaction.Rollback();
             }
-
-
         }
 
         private static void ExportEntities(VaporStoreDbContext context, string exportDir)
